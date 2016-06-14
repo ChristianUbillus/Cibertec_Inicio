2720c6cb0c70d01d15f50dd8aab0f2aba1ff9576
ChristianUbillus <christianubi4521@gmail.com>
2016-06-13 22:28:58 -0500

    13062016: Trabajo con Models, Conexion y mapeo con EntityFramework

---
+++ WebDeveloper.DataAccess/WebContextDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAccess
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb() : base("Name=WebDeveloperConnectionString")
+        {
+
+        }
+        public DbSet<Client> clients { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
