131f9454ed27460ff5f58dde3ea31d44e05dffaa
ChristianUbillus <christianubi4521@gmail.com>
2016-06-27 22:26:26 -0500

    27062016 - Routing

---
+++ WebDeveloper/ActionFilters/LogActionFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebDeveloper.ActionFilters
+{
+    public class LogActionFilter : ActionFilterAttribute
+    {
+        void Linea(string Opcion, RouteData RouteData )
+        {
+            Debug.WriteLine($"{Opcion} controller:{ RouteData.Values["controller"]} action:{ RouteData.Values["action"]}", "Action Filter Log");
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Linea("OnActionExecuting", filterContext.RouteData);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Linea("OnActionExecuted", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Linea("OnResultExecuted", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Linea("OnResultExecuting", filterContext.RouteData);
+        }
+    }
+}
--- WebDeveloper/Controllers/RazorController.cs
+++ WebDeveloper/Controllers/RazorController.cs
@@ -5,11 +5,16 @@ using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.Model;
 using WebDeveloper.DataAccess;
+using WebDeveloper.ActionFilters;
+
 namespace WebDeveloper.Controllers
 {
+    [LogActionFilter]
+    [RoutePrefix("Razor")]
     public class RazorController : Controller
     {
         // GET: Razor
+        [Route]
         public ActionResult Index()
         {
             var client = new Client()
@@ -26,5 +31,17 @@ namespace WebDeveloper.Controllers
             var productData = new ProductData();
             return View(productData.GetFakeProducts());
         }
+
+        [Route("Entry/{date:DateTime}")]
+        public ActionResult EntryDate(DateTime date)
+        {
+            ViewBag.Date = date.ToShortDateString();
+            return View();
+        }
+        [Route("Entry/{id:int}")]
+        public string EntryId(int id) {
+            return id.ToString();
+        }
+           
     }
 }
